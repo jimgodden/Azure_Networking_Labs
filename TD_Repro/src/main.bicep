11fb9237d93fb7b3d8b488a69dee563ebb60ae7f
jimgodden <james.c.godden@outlook.com>
2023-12-19 13:22:46 -0500

    switching to blob storage instead of file share

--- TD_Repro/src/main.bicep
+++ TD_Repro/src/main.bicep
@@ -99,6 +99,7 @@ module clientVM_Linux '../../modules/Microsoft.Compute/Ubuntu20/VirtualMachine.b
   dependsOn: [
     storageAccount
     client_StorageAccount_File_PrivateEndpoint
+    ServerVM_Linux
   ]
 } ]
 
@@ -135,7 +136,7 @@ module pcapReviewVM '../../modules/Microsoft.Compute/WindowsServer2022/VirtualMa
     virtualMachine_Size: 'Standard_B2ms'
     virtualMachine_ScriptFileLocation: virtualMachine_ScriptFileLocation
     virtualMachine_ScriptFileName: 'pcapreviewer.ps1'
-    commandToExecute: 'powershell.exe -ExecutionPolicy Unrestricted -File pcapreviewer.ps1 -ScenarioName ${scenario_Name} -StorageAccountName ${storageAccount.outputs.storageAccount_Name} -StorageAccountFileShareName ${storageAccount.outputs.storageAccountFileShare_Name} -StorageAccountKey ${storageAccount.outputs.storageAccount_key0}'
+    commandToExecute: 'powershell.exe -ExecutionPolicy Unrestricted -File pcapreviewer.ps1 -StorageAccountName ${storageAccount.outputs.storageAccount_Name} -StorageAccountKey ${storageAccount.outputs.storageAccount_key0}'
   }
   dependsOn: [
     clientVM_Linux // We only want to start checking for pcaps after the client has been deployed and starts capturing
--- scripts/pcapreviewer.ps1
+++ scripts/pcapreviewer.ps1
@@ -1,9 +1,19 @@
 Param(
     [string]$StorageAccountName,
     [string]$StorageAccountKey,
-    [string]$StorageAccountFileShareName,
-    [string]$ScenarioName
+    [string]$ContainerName
 )
+
+$filesToDownload = @(
+    "upload_to_blob.py",
+    "download_from_blob.py",
+    "delete_from_blob.py"
+)
+
+foreach ($fileToDownload in $filesToDownload) {
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jimgodden/Azure_Networking_Labs/main/scripts/$fileToDownload" -OutFile "c:\$fileToDownload"
+}
+
 
 # Chocolatey installation
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
@@ -14,77 +24,50 @@ Start-Job -ScriptBlock { choco install vscode -y }
 # Install Wireshark
 Start-Job -ScriptBlock { choco install wireshark -y }
 
+# Install Python 3.11
+Start-Job -ScriptBlock { choco install python311 -y }
+
 # Wait for all jobs to finish
 Get-Job | Wait-Job
 
-$StorageAccountNameShortened = $StorageAccountName.Split('.')
+pip install azure-storage-blob
 
-cmd.exe /C "cmdkey /add:`"${StorageAccountName}`" /user:`"localhost\$($StorageAccountNameShortened[0])`" /pass:`"${StorageAccountKey}`""
-New-PSDrive -Name Z -PSProvider FileSystem -Root "\\${StorageAccountName}\${StorageAccountFileShareName}" -Persist
-
-New-Item -ItemType Directory -Path "Z:\" -Name "iwashere"
+New-Item -Path C:\ -ItemType Directory -Name "captures"
+New-Item -Path C:\ -ItemType Directory -Name "possible"
+New-Item -Path C:\ -ItemType Directory -Name "no_problem"
 
 Start-Job -ScriptBlock {
     # Set the folder path to search
-    $folderPathBase = "Z:"
-    $folderPathOriginalPcaps = "Z:\$($using:ScenarioName)"
-
-    $folderForCapsWithoutIssues = "NoIssuesFound"
-
-    # Set the size threshold in megabytes
-    $sizeThreshold = 5
+    $folderPathBase = "C:"
+    $folderPathOriginalPcaps = "C:\captures"
 
     $filterCriteria = "tcp.flags.reset == 1 and tcp.time_delta < 7 and tcp.time_delta > 4"
     $tshark = "C:\Program Files\Wireshark\tshark.exe"
 
+    python.exe c:\download_from_blob.py --account-name $StorageAccountName --account-key $StorageAccountKey --container-name $ContainerName --local-path $folderPathOriginalPcaps
+
+
     while ($true) {
         # Get files in the specified folder that are greater than 5 megabytes
-        $files = Get-ChildItem -Path $folderPathOriginalPcaps | Where-Object { $_.Length -gt ($sizeThreshold * 1MB) }
+        $files = Get-ChildItem -Path $folderPathOriginalPcaps
 
         if ($files.Count -gt 0) {
-            Write-Host "Files larger than $sizeThreshold megabytes found in ${folderPathOriginalPcaps}:"
+            Write-Host "Found files."
             $files | ForEach-Object {
                 # Use tshark to filter packets based on the specified criteria
                 $tsharkOutput = "$tshark -r $($_.FullName) -Y ""$filterCriteria"""
 
                 if ($tsharkOutput) {
-                    Move-Item -Path $_.FullName -Destination $folderPathBase
+                    Move-Item -Path $_.FullName -Destination "${folderPathBase}/possible"
                 }
                 else {
-                    if (!(Test-Path "${folderPathBase}\${folderForCapsWithoutIssues}")) {
-                        New-Item -ItemType Directory -Path $folderPathBase -Name $folderForCapsWithoutIssues
-                    }
-                    Move-Item -Path $_.FullName -Destination "${folderPathBase}\${folderForCapsWithoutIssues}"
+                    Move-Item -Path $_.FullName -Destination "${folderPathBase}/no_problem"
                 }
             }
         } else {
-            Write-Host "No files larger than $sizeThreshold megabytes found in $folderPath."
+            Write-Host "No files found."
         }
 
         Start-Sleep -Seconds 1800
     } 
-}
-
-
-DefaultEndpointsProtocol=https;
-AccountName=mainjamesgstorage;
-AccountKey=;
-EndpointSuffix=core.windows.net
-
-
-# Define variables
-$sasUrl = "https://mainjamesgstorage.blob.core.windows.net/website?sp=racwdli&st=2023-12-15T21:02:50Z&se=2023-12-16T05:02:50Z&spr=https&sv=2022-11-02&sr=c&sig=O8iJU2wL7t0adKR8umoeudIpaMSBgVWlVKyGCI%2FaS8A%3D"
-
-# Get a list of blobs in the container
-$blobs = Invoke-RestMethod -Uri $sasUrl -Method Get
-
-# Delete each blob
-foreach ($blob in $blobs) {
-    $blobName = $blob.Name
-
-    # Construct the URL for blob deletion
-    $deleteUrl = "$sasUrl&restype=container&comp=blob&blob=$blobName"
-    
-    # Send a DELETE request to delete the blob
-    Invoke-RestMethod -Uri $deleteUrl -Method Delete
 }
