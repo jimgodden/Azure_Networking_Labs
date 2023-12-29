6faa311097a14db8a19cb8ac823c41d87d87f7db
jimgodden <james.c.godden@outlook.com>
2023-12-29 10:06:50 -0500

    testing removing start-jobs

--- scripts/ChocoInstalls.ps1
+++ scripts/ChocoInstalls.ps1
@@ -1,19 +1,24 @@
-# Install PowerShell Core
-Start-Job -ScriptBlock { choco install powershell-core -y }
+# # Install PowerShell Core
+# Start-Job -ScriptBlock { choco install powershell-core -y }
 
-# Install Python 3.11
-Start-Job -ScriptBlock { choco install python311 -y }
+# # Install Python 3.11
+# Start-Job -ScriptBlock { choco install python311 -y }
 
-# Install Visual Studio Code
-Start-Job -ScriptBlock { choco install vscode -y }
+# # Install Visual Studio Code
+# Start-Job -ScriptBlock { choco install vscode -y }
 
-# Install Wireshark
-Start-Job -ScriptBlock { choco install wireshark -y }
+# # Install Wireshark
+# Start-Job -ScriptBlock { choco install wireshark -y }
 
-# Install PsTools
-Start-Job -ScriptBlock { choco install pstools -y }
+# # Install PsTools
+# Start-Job -ScriptBlock { choco install pstools -y }
 
-# Wait for all jobs to finish
-Get-Job | Wait-Job
 
-Unregister-ScheduledTask -TaskName "ChocoInstalls" -Confirm:$false
+choco install powershell-core -y
+choco install python311 -y
+choco install vscode -y
+choco install wireshark -y
+choco install pstools -y
+
+
+# Unregister-ScheduledTask -TaskName "ChocoInstalls" -Confirm:$false
