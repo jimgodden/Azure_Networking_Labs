daa6545d2ae1217fc156bc47fd32e18e915e288c
jimgodden <james.c.godden@outlook.com>
2023-12-29 09:40:29 -0500

    testing

--- scripts/WinServ2022_ConfigScript_General.ps1
+++ scripts/WinServ2022_ConfigScript_General.ps1
@@ -5,4 +5,17 @@ Start-Job -ScriptBlock {
 
 Get-Job | Wait-Job
 
+# Creates a task that installs the tools when the user logs in
+$initTaskName = "Init"
+$initTaskAction = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File `"C:\WinServ2022_InstallTools.ps1`""
+$initTaskTrigger = New-ScheduledTaskTrigger -AtLogon
+Register-ScheduledTask -TaskName $initTaskName -Action $initTaskAction -Trigger $initTaskTrigger -Force
+
+# Creates a task that installs several packages using chocolatey after the computer has been restarted
+$currentTimePlusTwoMinutes = (Get-Date).AddMinutes(2)
+$chocoTaskName = "ChocoInstalls"
+$chocoTaskAction = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File `"C:\ChocoInstalls.ps1`""
+$chocoTaskTrigger = New-ScheduledTaskTrigger -Once -At $currentTimePlusTwoMinutes
+Register-ScheduledTask -TaskName $chocoTaskName -Action $chocoTaskAction -Trigger $chocoTaskTrigger -User "NT AUTHORITY\SYSTEM" -Force
+
 Restart-Computer
