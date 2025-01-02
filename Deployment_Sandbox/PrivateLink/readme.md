e663b215a7b2b9547a31819387e891c4ee074b0f
James Godden <jgodden90@outlook.com>
2025-01-02 17:04:58 -0500

    updating easy deploy buttons in readme

--- Deployment_Sandbox/PrivateLink/readme.md
+++ Deployment_Sandbox/PrivateLink/readme.md
@@ -1,6 +1,6 @@
 Lab environment for Azure Private Link
 
-[![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fjimgodden%2FAzure_Networking_Labs%2Fmain%2FSandbox-PrivateLink%2Fsrc%2Fmain.json)
+[![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fjimgodden%2FAzure_Networking_Labs%2Fmain%2FDeployment_Sandbox%2FPrivateLink%2Fsrc%2Fmain.json)
 
 
 Diagram of the infrastructure
---
+++ freebsdfrr.sh
@@ -0,0 +1,71 @@
+# https://docs.frrouting.org/projects/dev-guide/en/latest/building-frr-for-freebsd14.html
+
+pkg install -fy automake
+pkg install -fy bison
+pkg install -fy c-ares
+pkg install -fy git
+pkg install -fy gmake
+pkg install -fy json-c
+pkg install -fy libtool
+pkg install -fy libunwind
+pkg install -fy libyang2
+pkg install -fy pkgconf
+pkg install -fy protobuf-c
+pkg install -fy py311-pytest
+pkg install -fy py311-sphinx
+pkg install -fy texinfo
+
+pw groupadd frr -g 101
+pw groupadd frrvty -g 102
+pw adduser frr -g 101 -u 101 -G 102 -c "FRR suite" -d /usr/local/etc/frr -s /usr/sbin/nologin
+
+git clone https://github.com/frrouting/frr.git frr
+cd frr
+./bootstrap.sh
+export MAKE=gmake LDFLAGS=-L/usr/local/lib CPPFLAGS=-I/usr/local/include
+./configure \
+    --sysconfdir=/usr/local/etc \
+    --localstatedir=/var \
+    --enable-pkgsrcrcdir=/usr/pkg/share/examples/rc.d \
+    --prefix=/usr/local \
+    --enable-multipath=64 \
+    --enable-user=frr \
+    --enable-group=frr \
+    --enable-vty-group=frrvty \
+    --enable-configfile-mask=0640 \
+    --enable-logfile-mask=0640 \
+    --enable-fpm \
+    --with-pkg-git-version \
+    --with-pkg-extra-version=-MyOwnFRRVersion
+gmake
+gmake check
+sudo gmake install
+
+
+mkdir /usr/local/etc/frr
+touch /usr/local/etc/frr/frr.conf
+
+sudo touch /usr/local/etc/frr/babeld.conf
+sudo touch /usr/local/etc/frr/bfdd.conf
+sudo touch /usr/local/etc/frr/bgpd.conf
+sudo touch /usr/local/etc/frr/eigrpd.conf
+sudo touch /usr/local/etc/frr/isisd.conf
+sudo touch /usr/local/etc/frr/ldpd.conf
+sudo touch /usr/local/etc/frr/nhrpd.conf
+sudo touch /usr/local/etc/frr/ospf6d.conf
+sudo touch /usr/local/etc/frr/ospfd.conf
+sudo touch /usr/local/etc/frr/pbrd.conf
+sudo touch /usr/local/etc/frr/pimd.conf
+sudo touch /usr/local/etc/frr/ripd.conf
+sudo touch /usr/local/etc/frr/ripngd.conf
+sudo touch /usr/local/etc/frr/staticd.conf
+sudo touch /usr/local/etc/frr/zebra.conf
+sudo chown -R frr:frr /usr/local/etc/frr/
+sudo touch /usr/local/etc/frr/vtysh.conf
+sudo chown frr:frrvty /usr/local/etc/frr/vtysh.conf
+sudo chmod 640 /usr/local/etc/frr/*.conf
+
+echo "net.inet.ip.forwarding=1" | sudo tee -a /etc/sysctl.conf
+echo "net.inet6.ip6.forwarding=1" | sudo tee -a /etc/sysctl.conf
+sysctl net.inet.ip.forwarding=1
+sysctl net.inet6.ip6.forwarding=1
